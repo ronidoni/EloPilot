59a029ff4850e1847adcd2120be6d866a16f2bf6
ronidoni <rakog0336@gmail.com>
2026-09-18 21:22:10 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { GoogleGenAI } = require('@google/genai');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
